a6ed2f74818fc2f8aa0e3af9fa98de125a30e7eb
56940 <56940@LAPTOP-3LGDAN5T>
2023-05-02 16:06:32 -0400

    Agregar archivos de proyecto.

---
+++ Clientee/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicitar IP del servidor y puerto al que conectarse
+            Console.Write("Ingrese la IP del Servidor: ");
+            string ip = Console.ReadLine();
+            Console.Write("Ingrese el puerto: ");
+            int port = int.Parse(Console.ReadLine());
+
+            // Conectar con el servidor
+            TcpClient client = new TcpClient(ip, port);
+            Console.WriteLine("Conectado al Servidor...");
+
+            // Obtener el stream de entrada y salida de datos del servidor
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                // Enviar mensaje al servidor
+                Console.Write("Cliente dice: ");
+                string mensaje = Console.ReadLine();
+                byte[] data = Encoding.ASCII.GetBytes(mensaje);
+                stream.Write(data, 0, data.Length);
+
+
+                if (mensaje == "chao")
+                {
+                    Console.WriteLine("Desconectado del Servidor...");
+                    stream.Close();
+                    client.Close();
+                    break;
+                }
+                byte[] buffer = new byte[1024];
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+                mensaje = Encoding.ASCII.GetString(buffer, 0, bytes);
+                Console.WriteLine("Servidor dice: " + mensaje);
+            }
+        }
+    }
+}
---
+++ sumativa2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumativa2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Establecer el socket TCP en el puerto 2050
+            TcpListener servidor = new TcpListener(IPAddress.Any, 2050);
+            servidor.Start();
+            Console.WriteLine("Servidor iniciado...");
+
+            while (true)
+            {
+                // Esperar una conexión entrante
+                TcpClient client = servidor.AcceptTcpClient();
+                Console.WriteLine("Cliente conectado...");
+
+                // Obtener el stream de entrada y salida de datos del cliente
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    // Recibir el mensaje enviado por el cliente
+                    byte[] buffer = new byte[1024];
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string mensaje = Encoding.ASCII.GetString(buffer, 0, bytes);
+                    Console.WriteLine("Cliente dice: " + mensaje);
+
+                    // Si el mensaje es "chao", cerrar la conexión con el cliente
+                    if (mensaje == "chao")
+                    {
+                        Console.WriteLine("Cliente se desconectó...");
+                        stream.Close();
+                        client.Close();
+                        break;
+                    }
+
+                    // Enviar mensaje de vuelta al cliente
+                    Console.Write("Servidor dice: ");
+                    mensaje = Console.ReadLine();
+                    byte[] data = Encoding.ASCII.GetBytes(mensaje);
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+        }
+    }
+}
